1a426337cd76eee53f2469bfb4ef3c0d3e54d375
eugbon <eugbon@a1411>
2018-02-01 18:14:50 +0100

    soy tu padre

--- Aceptaelreto/src/aceptaelreto/Aceptaelreto.java
+++ Aceptaelreto/src/aceptaelreto/Aceptaelreto.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package aceptaelreto;
 
-/**
- *
- * @author eugbon
- */
+package aceptaelreto;
+import java.util.Scanner;
+
 public class Aceptaelreto {
 
     /**
@@ -16,8 +9,31 @@ public class Aceptaelreto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner Scan=new Scanner(System.in);
         
-        System.out.println("aceptaelreto2hfgthgfh7777722");
+        
+        String texto0;
+        String texto1;
+        int casos;
+        int i;
+        
+        
+        casos=Scan.nextInt();
+        //El scan me da un salto de linea
+        Scan.nextLine();
+        for(i=0;i<casos;i++){
+            //System.out.println("Dime: ");
+            texto0 = Scan.nextLine();
+            texto1 = Scan.nextLine();
+            
+            //System.out.println("Dime dime: ");
+            if(texto0.equals("Luke")&&texto1.equals("padre")){
+                    System.out.println("TOP SECRET");
+            }else{
+                
+                System.out.println( texto0 + ", yo soy tu " + texto1);
+            }
+        }
     }
     
 }
